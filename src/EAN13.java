7283704963a61cf0ea4248a47416cd29e282e380
TorFra <francescomaria.tornese@itcgtursi.edu.it>
2023-05-10 10:24:45 +0200

    Inserimento della documentazione nel file EAN13.java

--- src/EAN13.java
+++ src/EAN13.java
@@ -1,17 +1,36 @@
+/**
+ *@author Francesco Tornese 5inf
+ *@version 1.0.0
+ *@since 10/05/2023
+ */
+
+
+
 public class EAN13 {
-    
+    /**
+     * codice è una srringa che andrà a contenere il notro dato in questo caso un EAN
+     */
     private String codice;
 
-   
+   /**
+    * 
+    * @param c è il dato a cui viene assengato "codice"
+    */
     public void setCodice(String c){
         codice = c;
     }
-
+    /**
+     * 
+     * @return ci fa ritornare il valore inserito in "codice"
+     */
     public String getCodice(){
         return codice;
     }
 
-
+    /**
+     * qui abbiamo una procedura che controlla che il codice inserito sia vuoto oppure non valido
+     * @return questa procedura ritorna false se il dato inserito ha valore diverso da 13
+     */
     public boolean verificaCodice(){
 
         // Verifica se l'attributo codice è vuoto o nullo
